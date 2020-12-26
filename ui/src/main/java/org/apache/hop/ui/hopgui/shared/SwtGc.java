242619de7d276951e242e99989e3d34ec1563cdc
Hiromu Hota <hiromu.hota@gmail.com>
2020-12-25 17:08:16 -0800

    GC.setLineStyle is not implemented in RAP

--- ui/src/main/java/org/apache/hop/ui/hopgui/shared/SwtGc.java
+++ ui/src/main/java/org/apache/hop/ui/hopgui/shared/SwtGc.java
@@ -351,26 +351,7 @@ public class SwtGc implements IGc {
   }
 
   public void setLineStyle( ELineStyle lineStyle ) {
-    switch ( lineStyle ) {
-      case DASHDOT:
-        gc.setLineStyle( SWT.LINE_DASHDOT );
-        break;
-      case SOLID:
-        gc.setLineStyle( SWT.LINE_SOLID );
-        break;
-      case DOT:
-        gc.setLineStyle( SWT.LINE_DOT );
-        break;
-      case DASH:
-        gc.setLineStyle( SWT.LINE_DASH );
-        break;
-      case PARALLEL:
-        gc.setLineAttributes( new LineAttributes(
-          gc.getLineWidth(), SWT.CAP_FLAT, SWT.JOIN_MITER, SWT.LINE_CUSTOM, new float[] { 5, 3, }, 0, 10 ) );
-        break;
-      default:
-        break;
-    }
+    return;
   }
 
   public void setLineWidth( int width ) {
